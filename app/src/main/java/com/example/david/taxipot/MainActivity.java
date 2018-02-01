62046386fe8a07eece96fb6841f4fd56be48bb20
qkrgusdn159 <35929152+qkrgusdn159@users.noreply.github.com>
2018-02-01 14:27:49 +0900

    로그인 구현완료

--- app/src/main/java/com/example/david/taxipot/MainActivity.java
+++ app/src/main/java/com/example/david/taxipot/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.david.taxipot;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        Button button = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
